4ec682106bd14e8a77a72edf61831b1b9edee152
Mikov Denis <denis.mikov.06@gmail.com>
2025-05-30 09:28:50 +0300

    Initial commit with echo command setup

---
+++ CommandEcho.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenMod.Core.Commands;
+
+namespace Scitalis.Analytics
+{
+    [Command("echo")]
+    [CommandDescription("Echo a message")]
+    [CommandSyntax("<message>")]
+    public class CommandEcho : Command
+    {
+        public CommandEcho(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            
+        }
+
+        protected override async Task OnExecuteAsync()
+        {        
+            // This gets us the text that the user wants to echo.
+            string text = string.Join(" ", Context.Parameters);
+            
+            await Context.Actor.PrintMessageAsync(text);
+        }
+    }
+}
